f978ff64a8447118d40c6c3f98cd3a8c00639418
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-03-26 09:47:52 +0900

    props 및 emit을 활용한 예제

---
+++ 07_Props/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import PropsParents from './components/01_propBasic/PropsParents.vue';
+</script>
+
+<template>
+    <header>
+        <PropsParents/>
+    </header>
+
+    <main>
+        
+    </main>
+</template>
+
+<style scoped>
+
+</style>
